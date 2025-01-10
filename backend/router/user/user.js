adac65aaeb90505eebfd37d34dcf5b5aca4efdb1
abdikadir qulle <wlaaldaadaa33@gmail.com>
2025-01-10 13:16:28 +0300

    commit anythings.

--- backend/router/user/user.js
+++ backend/router/user/user.js
@@ -1,27 +1,27 @@
-import express, { Router } from "express";
-import {deleteUser, loginUser, registerUser, signout } from "../../controller/user/userController.js";
-import { connectDB, prisma } from "../../config/db.js";
-import { Authoriza } from "../../middleware/middleware.js";
-const userRouter = express.Router();
+import express, { Router } from "express"
+import {
+  deleteUser,
+  loginUser,
+  registerUser,
+  signout,
+} from "../../controller/user/userController.js"
+import { connectDB, prisma } from "../../config/db.js"
+import { Authoriza } from "../../middleware/middleware.js"
+const userRouter = express.Router()
 
-userRouter.get('/', async (req , res) => {
-    await connectDB;
-    const users = await prisma.user.findMany()
+userRouter.get("/", async (req, res) => {
+  const users = await prisma.user.findMany()
 
-    res.status(200).json({ users});
-
+  await connectDB
+  res.status(200).json({ users })
 })
 
-userRouter.post('/register-user', registerUser);
-userRouter.post('/login-user', loginUser);
-userRouter.get('/signout', signout);
-userRouter.delete('/deleteUser/:id', deleteUser);
-userRouter.get('/current-user', Authoriza, (req, res) => {
-    res.json(req.user);
-  });
+userRouter.post("/register-user", registerUser)
+userRouter.post("/login-user", loginUser)
+userRouter.get("/signout", signout)
+userRouter.delete("/deleteUser/:id", deleteUser)
+userRouter.get("/current-user", Authoriza, (req, res) => {
+  res.json(req.user)
+})
 
-export default userRouter;
-
-
-
-
+export default userRouter
